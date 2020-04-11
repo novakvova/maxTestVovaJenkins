e2bac62b535a4d5de3b11a4cfd151d2ec70dd219
MaksumNechiporuk <maksum.nec@gmail.com>
2020-04-11 18:36:53 +0200

    add css

--- CarSale/ClientApp/src/App.js
+++ CarSale/ClientApp/src/App.js
@@ -1,11 +1,11 @@
 import React, {Fragment} from 'react';
 import { Route } from 'react-router';
+import "./App.css"
 
 import CarList from "./components/CarList/CarList"
 
 export default () => (
     <Fragment>
-        <h1>CarSale</h1>
-
+        <CarList/>
     </Fragment>
 );
