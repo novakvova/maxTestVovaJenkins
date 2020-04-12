0540e1d9e7c1a71a0bd23bdbfb8e9b6f660082e9
MaksumNechiporuk <maksum.nec@gmail.com>
2020-04-12 17:34:12 +0200

    add about

--- CarSale/ClientApp/src/App.js
+++ CarSale/ClientApp/src/App.js
@@ -2,9 +2,11 @@ import React, {Fragment} from 'react';
 import { Route } from 'react-router';
 import './css/main.css'
 import CarList from "./components/CarList/CarList"
+import About from "./components/About/About"
 
 export default () => (
     <Fragment>
-        <CarList/>
+        <CarList />
+        <About/>
     </Fragment>
 );
