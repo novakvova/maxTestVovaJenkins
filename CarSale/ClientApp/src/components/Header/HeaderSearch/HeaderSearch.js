0bda43e22a579b5a12d99ac1a973c27ba71bcd59
tereshkovychura@gmail.com <tereshkovychura@gmail.com>
2020-04-17 16:12:26 +0300

    Change class to classname

--- CarSale/ClientApp/src/components/Header/HeaderSearch/HeaderSearch.js
+++ CarSale/ClientApp/src/components/Header/HeaderSearch/HeaderSearch.js
@@ -3,60 +3,60 @@ class HeaderSearch extends Component {
    
     render() {
         return (
-            <div class="container-fluid header-search ">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-4 offset-lg-8 col-md-5 offset-md-8">
-                            <div class="search-part">
-                                <h2 class="header-text">Search Pre-Owned Cars</h2>
-                                <form class="pre-owned-form">
-                                    <div class="select-group">
-                                        <select class="select-item">
+            <div className="container-fluid header-search ">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-4 offset-lg-8 col-md-5 offset-md-8">
+                            <div className="search-part">
+                                <h2 className="header-text">Search Pre-Owned Cars</h2>
+                                <form className="pre-owned-form">
+                                    <div className="select-group">
+                                        <select className="select-item">
                                             <option hidden value=""> Make</option>
                                             <option value=""> Audi</option>
                                             <option value=""> BMV</option>
                                         </select>
-                                        <select class="select-item">
+                                        <select className="select-item">
                                             <option hidden value=""> Model</option>
                                             <option value=""> Audi</option>
                                             <option value=""> BMV</option>
                                         </select>
-                                        <select class="select-item">
+                                        <select className="select-item">
                                             <option hidden value=""> Year</option>
                                             <option value=""> Audi</option>
                                             <option value=""> BMV</option>
                                         </select>
-                                        <select class="select-item">
+                                        <select className="select-item">
                                             <option hidden value=""> Body Type</option>
                                             <option value=""> Audi</option>
                                             <option value=""> BMV</option>
                                         </select>
-                                        <select class="select-item">
+                                        <select className="select-item">
                                             <option hidden value=""> Min Price</option>
                                             <option value=""> Audi</option>
                                             <option value=""> BMV</option>
                                         </select>
-                                        <select class="select-item">
+                                        <select className="select-item">
                                             <option hidden value=""> Max Price</option>
                                             <option value=""> Audi</option>
                                             <option value=""> BMV</option>
                                         </select>
-                                        <select class="select-item">
+                                        <select className="select-item">
                                             <option hidden value=""> Color</option>
                                             <option value=""> Audi</option>
                                             <option value=""> BMV</option>
                                         </select>
-                                        <button type="submit" class=" btn btn_search">Search</button>
-                                        <button type="submit" class=" btn btn_reset">reset</button>
+                                        <button type="submit" className=" btn btn_search">Search</button>
+                                        <button type="submit" className=" btn btn_reset">reset</button>
                                     </div>
-                                    <div class="buttons-group">
-                                        <button type="submit" class=" btn btn_buy-car">
-                                            <img src="../../img/car-png.png" class="btn_buy-car-image" />
+                                    <div className="buttons-group">
+                                        <button type="submit" className=" btn btn_buy-car">
+                                            <img src="../../img/car-png.png" className="btn_buy-car-image" />
                                             Buy <br />
                                             Used Cars
                                         </button>
-                                        <button type="submit" class=" btn btn_sell-car">
-                                            <img src="../../img/key-png.png" class="btn_sell-car-image" />
+                                        <button type="submit" className=" btn btn_sell-car">
+                                            <img src="../../img/key-png.png" className="btn_sell-car-image" />
                                             Sell <br />
                                             Your Car
                                         </button>
