e1621ed52e3888cc1f22e8cfac0ce7b912527bd1
KhraschukZahar <antisocialll@ukr.net>
2020-04-20 20:16:38 +0300

    added registration form

--- CarSale/ClientApp/src/App.js
+++ CarSale/ClientApp/src/App.js
@@ -5,12 +5,14 @@ import CarList from "./components/CarList/CarList"
 import About from "./components/About/About"
 import EditProfile from "./components/Profile/EditProfile/EditProfile"
 import ShowProfile from "./components/Profile/ShowProfile/ShowProfile"
+import Registration from "./components/Registration/Registration"
 
 export default () => (
 	<Fragment>
 		<CarList />
    <About/>
 		<EditProfile />
-		<ShowProfile />
+        <ShowProfile />
+        <Registration />
 	</Fragment>
 );
